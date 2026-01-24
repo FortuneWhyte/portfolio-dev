cac64257486db7619a9dffc2d11726dc427dc173
FortuneWhyte <greatfortunewhyte@yahoo.com>
2026-01-24 13:12:58 -0600

    Restructure project as single-page portfolio with sections

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+export default function  Hero() {
+    return (
+        <section id="home" class="hero">
+            <div class="hero-content">
+                <h1>Fortune Whyte</h1>
+                <p>Hi, I'm Great-Fortune Abiye-Whyte, a software developer with a strong foundation in C++, specializing in modern web development and user-focused applications.
+                    I'm passionate about building digital experiences that blend creativity with technology. Outside of programming, I enjoy playing football and going to the gym.
+                </p>
+            </div>
+            
+
+        </section>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav class="navbar">
+            <a href="#home" class="logo">SoftwareDev</a>
+            <ul class="nav-links">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
