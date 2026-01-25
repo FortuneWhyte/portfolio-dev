1069a5edbf1f79223dca1f4aee4f0e4eb5d5274b
FortuneWhyte <greatfortunewhyte@yahoo.com>
2026-01-25 16:49:34 -0600

    Add contact components

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+    return (
+        <section id="contact">
+            <h2>Let's get in touch</h2>
+            <p>Have an idea or if you're interested in working together? Send me a message!</p>
+
+            
+
+        </section>
+    );
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,31 @@
 import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 export default function Footer() {
-    
+    return (
+    <footer>
+      <p>&copy; 2026 Great-Fortune Abiye-Whyte. All rights reserved.</p>
+
+      <div className="social-links">
+        <a href="https://github.com/FortuneWhyte" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/great-fortune-abiye-whyte-8a91a2280/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+
+        <a
+          href="https://www.facebook.com/whyte.fortuneee/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook />
+        </a>
+      </div>
+    </footer>
+  );
+}
