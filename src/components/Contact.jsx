6a37be426578bb2159954fb2742a7a51cd1f04e0
FortuneWhyte <greatfortunewhyte@yahoo.com>
2026-01-30 13:29:14 -0600

    Styling footer section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <section id="contact">
-            <h2>Let's get in touch</h2>
+            <h2>Get in touch!</h2>
             <p>Have an idea or if you're interested in working together? Send me a message!</p>
 
             <form>
