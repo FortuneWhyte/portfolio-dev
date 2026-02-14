035f78cc4c51b69ed21440b627aba78b142e8d55
FortuneWhyte <greatfortunewhyte@yahoo.com>
2026-02-14 15:17:28 -0600

    Styling of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,40 +8,40 @@ export default function About() {
 
       <section className="about-page">
         <div className="page">
-            <div className="container about-page">
-                <div class="text-zone">
-                    <h1>
-                        <span>
-                            <span class="text-animate-hover _15">A</span>
-                            <span class="text-animate-hover _15">b</span>
-                            <span class="text-animate-hover _15">o</span>
-                            <span class="text-animate-hover _15">u</span>
-                            <span class="text-animate-hover _15">t</span>
-                            <span class="text-animate-hover _15"> </span>
-                            <span class="text-animate-hover _15">m</span>
-                            <span class="text-animate-hover _15">e</span>
-                        </span>
-                    </h1>
-                        <p> 
-                            I am a driven software developer with a diverse background that has 
-                            shaped my ability to collaborate effectively, communicate clearly, 
-                            and approach technical challenges with strong analytical thinking.
-                            <br>
-                                <div>
-                                    <h2>Languages</h2>
-                                        <ul>
-                                            <li>
-                                                <span>"C++: 4 years"</span>
-                                                <div className="progress-bar C++-progress">
-                                                    <div className="progress" style="width: 100%"></div>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                </div>
-                            </br>
-                        </p>
-                </div>
+          <div className="container about-page">
+            <div className="text-zone">
+              <h1>
+                <span>
+                  <span className="text-animate-hover _15">A</span>
+                  <span className="text-animate-hover _15">b</span>
+                  <span className="text-animate-hover _15">o</span>
+                  <span className="text-animate-hover _15">u</span>
+                  <span className="text-animate-hover _15">t</span>
+                  <span className="text-animate-hover _15"> </span>
+                  <span className="text-animate-hover _15">m</span>
+                  <span className="text-animate-hover _15">e</span>
+                </span>
+              </h1>
+
+              <p>
+                I am a driven software developer with a diverse background that has
+                shaped my ability to collaborate effectively, communicate clearly,
+                and approach technical challenges with strong analytical thinking.
+              </p>
+
+              <div>
+                <h2>Languages</h2>
+                <ul>
+                  <li>
+                    <span>C++: 4 years</span>
+                    <div className="progress-bar C++-progress">
+                      <div className="progress" style={{ width: "100%" }}></div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
+          </div>
         </div>
       </section>
 
